62bf58a16268852acd8e06cb9de6615559bcf2e1
masted <masted311@gmail.com>
2014-05-06 13:01:39 +0400

    Commit was made from server localhost by ci/push

--- static/js/Ngn.Dialog.NewPage.js
+++ static/js/Ngn.Dialog.NewPage.js
@@ -1,3 +1,23 @@
+Ngn.Dialog.NewPageBase = new Class({
+  Extends: Ngn.Dialog.RequestForm,
+  
+  options: {
+    controllerPath: '/admin/pages'
+  },
+  
+  getOpt: function() {
+    return {
+      getFormData: function() {
+        var data = Ngn.frm.toObj(this.form.eForm);
+        data.parentId = this.options.pageId;
+        return data;
+      }
+    };
+  }
+  
+});
+
+
 Ngn.Dialog.NewPage = new Class({
   Extends: Ngn.Dialog.NewPageBase,
 
@@ -13,5 +33,40 @@ Ngn.Dialog.NewPage = new Class({
     Ngn.frm.initTranslateField('titlei', 'namei');
     this.message.getElement('input[name=title]').focus();
   }
+  
+});
+
+Ngn.Dialog.NewModulePage = new Class({
+  Extends: Ngn.Dialog.NewPageBase,
+  
+  actionName: 'newModulePage',
+  
+  initialize: function(_opts) {
+    this.parent($merge(_opts, this.getOpt(), {
+      title: 'Создание нового раздела',
+      url: this.options.controllerPath + '/' + _opts.pageId + '/json_' + this.actionName
+    }));
+  },
+  
+  formResponse: function(r) {
+    this.parent(r);
+    this.message.getElement('input[name=title]').focus();
+    this.afterFormResponse();
+  },
+  
+  afterFormResponse: function() {
+    Ngn.frm.initTranslateField('titlei', 'namei');
+    Ngn.frm.initCopySelectValue('modulei', 'namei');
+    Ngn.frm.initCopySelectTitle('modulei', 'titlei');
+  }
+  
+});
+
+Ngn.Dialog.NewModulePageSimple = new Class({
+  Extends: Ngn.Dialog.NewModulePage,
+  
+  actionName: 'newModulePageSimple',
+  
+  afterFormResponse: function() {}
   
 });
