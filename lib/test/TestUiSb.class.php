093fff8ff34871a88af00c2545aa6af8517055e9
masted <masted311@gmail.com>
2014-10-05 15:23:19 +0400

    Auto-commit from localhost

--- lib/test/TestUiSb.class.php
+++ lib/test/TestUiSb.class.php
@@ -2,6 +2,10 @@
 
 abstract class TestUiSb extends TestCasperProjectAbstract {
 
+  static function enable() {
+    return false;
+  }
+
   protected function casper(array $steps, array $options = []) {
     Casper::run(PROJECT_KEY, $steps, array_merge([
       'extension' => SB_PATH.'/casper/extention.js'
